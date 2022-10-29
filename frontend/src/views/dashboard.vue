8c894eb5975e438b94a02baac471645a98f00eea
Krystyan Severin <krystyanseverin@gmail.com>
2022-10-29 11:05:04 -0500

    Created dashboard as view and no longer as a component

---
+++ frontend/src/views/dashboard.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <div>
+      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
+    </div>
+    <div>
+      <BarChart :chartData="chartData" :loaded="loaded" />
+    </div>
+    <div>
+      <EventTable :eventData="eventData"/>
+    </div>
+  </main>
+</template>
+<script>
+import BarChart from "../components/dashboard/barchart.vue";
+import EventTable from "../components/dashboard/eventAttendeeTable.vue";
+import axios from "axios";
+export default {
+  components: { BarChart,EventTable },
+  data() {
+    return {
+      eventData: {},
+      loaded: false,
+      chartData: {
+        datasets: [
+          {
+            label: "Total Attendees",
+            backgroundColor: "#f87979",
+            barThickness: 50
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    let apiURL = import.meta.env.VITE_ROOT_API + "/eventdata/attendees";
+    axios.get(apiURL).then((res) => {
+      this.eventData = res.data;
+      let labels = [];
+      let data = [];
+      res.data.forEach(eventDetail => {
+        labels.push(eventDetail.eventName);
+        data.push(eventDetail.totalAttendees);
+      });
+      this.chartData.labels = labels;
+      this.chartData.datasets[0].data = data;
+    })
+    this.loaded = true;
+  }
+};
+</script>
